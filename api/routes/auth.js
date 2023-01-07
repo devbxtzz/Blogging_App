a3b5614299efead0e2108422d9dfbaea7dd25a40
devbxtzz <batztonnie@gmail.com>
2023-01-07 20:47:44 +0300

    Updated Routes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -5,7 +5,14 @@ const bcrypt = require("bcrypt");
 
 router.post("/register" ,async (req, res) => {
     try { 
+        const salt = await bcrypt.genSalt(10);
+        const hashedPass = await bcrypt.hash(req.body.password, salt);
 
+        const newUser = new User({
+            user: req.body.username,
+            email: req.body.email,
+            password: hashedPass,
+        })
     } catch(error) {
         console.log(error)
     }
