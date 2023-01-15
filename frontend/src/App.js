d2b37a466e1badcc1f027c9e7f34b4d0b80f0e85
devbxtzz <batztonnie@gmail.com>
2023-01-15 13:41:44 +0300

    Client site Updates

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">
+          UpBlog
+        </a>
+        <nav>
+          <a href="">Sign in</a>
+          <a href="">Sign Up</a>
+        </nav>
+      </header>
+    </main>
+  );
+}
+
+export default App;
